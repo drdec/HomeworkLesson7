03baaf1f1b9d55fef40393dd5b0d988d142f8f27
DrDeckto <fynjy138@gmail.com>
2022-04-12 23:23:35 +0300

    Реализованно Дз 1

---
+++ HomeworkLesson7/Coder/BCoder.cs
@@ -0,0 +1,61 @@
+﻿namespace HomeworkLesson7
+{
+    public class BCoder : ICoder
+    {
+        private string _str;
+
+        public BCoder(string str)
+        {
+            _str = str;
+        }
+
+        /// <summary>
+        /// декодирует строку
+        /// </summary>
+        public void Decode()
+        {
+            var str = _str.ToCharArray();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 65 && str[i] <= 90)
+                {
+                    str[i] = (char)(65 + (90 - (int)str[i]));
+                }
+                else if (str[i] >= 97 && str[i] <= 122)
+                {
+                    str[i] = (char) (97 + (122 - (int) str[i]));
+                }
+            }
+
+            _str = new string(str);
+        }
+
+        /// <summary>
+        /// кодирует строку
+        /// </summary>
+        public void Encode()
+        {
+            var str = _str.ToCharArray();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] >= 65 && str[i] <= 90)
+                {
+                    str[i] = (char)(90 - ((int)str[i] - 65));
+                }
+                else if (str[i] >= 97 && str[i] <= 122)
+                {
+                    str[i] = (char)(122 - ((int)str[i] - 97));
+                }
+            }
+
+            _str = new string(str);
+        }
+
+        public override string ToString()
+        {
+            return _str;
+        }
+    }
+}
---
+++ HomeworkLesson7/Test.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkLesson7
+{
+    internal class Test
+    {
+        private Test() { }
+
+        public static void TestCoder()
+        {
+            var coderA = new ACoder("Alex");
+
+            Console.WriteLine(coderA);
+            coderA.Encode();
+            Console.WriteLine(coderA);
+
+            coderA.Decode();
+            Console.WriteLine(coderA);
+
+            Console.WriteLine("==================================");
+
+            var coder = new BCoder("ALEX");
+            Console.WriteLine(coder);
+
+            coder.Encode();
+            Console.WriteLine(coder);
+
+            coder.Decode();
+            Console.WriteLine(coder);
+
+            Console.WriteLine("==================================");
+
+            var coder1 = new BCoder("alex");
+            Console.WriteLine(coder1);
+
+            coder1.Encode();
+            Console.WriteLine(coder1);
+
+            coder1.Decode();
+            Console.WriteLine(coder1);
+        }
+    }
+}
